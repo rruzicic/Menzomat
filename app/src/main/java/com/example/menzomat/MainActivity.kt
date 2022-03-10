ab3486e06e761373f5812889d24fa700dfb1e8e8
Ratko Ruzicic <rruzicic@gmail.com>
2022-03-10 15:54:34 +0100

    Add shared preferences to breakfast, lunch and dinner fields

--- app/src/main/java/com/example/menzomat/MainActivity.kt
+++ app/src/main/java/com/example/menzomat/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.menzomat
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -24,7 +27,40 @@ class MainActivity : AppCompatActivity() {
                 ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
             }
         }
+
+        // retrieve data upon reopening the app
+        val cntDorucak = findViewById<EditText>(R.id.cntDorucak)
+        val cntRucak = findViewById<EditText>(R.id.cntRucak)
+        val cntVecera = findViewById<EditText>(R.id.cntVecera)
+
+        val pref = getPreferences(Context.MODE_PRIVATE)
+
+        val dorucak_cnt = pref.getInt("dorucak_cnt", 0)
+        val rucak_cnt = pref.getInt("rucak_cnt", 0)
+        val vecera_cnt = pref.getInt("vecera_cnt", 0)
+
+        cntDorucak.setText(dorucak_cnt.toString())
+        cntRucak.setText(rucak_cnt.toString())
+        cntVecera.setText(vecera_cnt.toString())
     }
+    override fun onPause() {
+        super.onPause()
+
+        // save the data for being used the next time
+        val cntDorucak = findViewById<EditText>(R.id.cntDorucak)
+        val cntRucak = findViewById<EditText>(R.id.cntRucak)
+        val cntVecera = findViewById<EditText>(R.id.cntVecera)
+
+        val pref = getPreferences(Context.MODE_PRIVATE)
+        val editor = pref.edit()
+
+        editor.putInt("dorucak_cnt", cntDorucak.getText().toString().toInt())
+        editor.putInt("rucak_cnt", cntRucak.getText().toString().toInt())
+        editor.putInt("vecera_cnt", cntVecera.getText().toString().toInt())
+
+        editor.commit()
+    }
+
 
     // requesting permissions
     override fun onRequestPermissionsResult(
